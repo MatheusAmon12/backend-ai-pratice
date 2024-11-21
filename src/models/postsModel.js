4a2bcf6e007ecaa4cf9f327cebf89a0e09acceaf
Matheus Amon <amonmatheus757@gmail.com>
2024-11-21 20:42:47 -0300

    FEAT: add posts model

---
+++ src/models/postsModel.js
@@ -0,0 +1,14 @@
+import connectToDatabase from "../config/dbConfig.js";
+
+const databaseUrl = process.env.DATABASE_URL;
+const mongoClientConnection = await connectToDatabase(databaseUrl);
+
+const getAllPosts = async() => {
+    const db = mongoClientConnection.db("project_instabytes");
+    const postsColletions = db.collection("posts");
+    const posts = await postsColletions.find({}).toArray();
+};
+
+export {
+    getAllPosts,
+}
--- src/routes/posts.routes.js
+++ src/routes/posts.routes.js
@@ -1,24 +1,10 @@
 import express from "express";
-import posts from "../model/posts.js";
-import { getPosts } from "../controllers/posts/getAllPosts.js";
-
-const getPostById = (id) => {
-    return posts.find(post => post.id === Number(id));
-}
+import { postsList } from "../controllers/postsController.js";
 
 const routes = (app) => {
     app.use(express.json());
 
-    app.get("/", async (req, res) => {
-        const posts = await getPosts();
-        res.status(200).json(posts);
-    });
-    
-    app.get("/:id", (req, res) => {
-        const id = req.params.id;
-        const result = getPostById(id);
-        res.status(200).json(result);
-    });
+    app.get("/", postsList);
 }
 
 export default routes;
