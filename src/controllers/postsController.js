ea4e78e2c339013c9bfe9bfbd6dba8c8f07ad913
Matheus Amon <amonmatheus757@gmail.com>
2024-11-24 11:47:39 -0300

    FEAT: add image upload

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -21,7 +21,28 @@ const addNewPost = async (req, res) => {
     }
 };
 
+const imageUpload = async (req, res) => {
+    const newPost = {
+        description: "",
+        imgUrl: req.file.originalname,
+        alt: ""
+    };
+    try {
+        const createdPost = await createPost(newPost);
+        res.status(201).json({
+            createdPost, 
+            message: "Post criado com sucesso!"
+        });
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).json({
+            message: "Ocorreu um erro ao criar o post",
+        });
+    }
+};
+
 export {
     postsList,
     addNewPost,
+    imageUpload
 }
--- src/routes/posts.routes.js
+++ src/routes/posts.routes.js
@@ -1,12 +1,15 @@
 import express from "express";
-import { addNewPost, postsList} from "../controllers/postsController.js";
+import { addNewPost, imageUpload, postsList} from "../controllers/postsController.js";
+import { upload } from "../middlewares/setUploadedFileDest.js";
 
-const routes = (app) => {
+const routes = (app) => {   
     app.use(express.json());
 
     app.get("/posts", postsList);
 
     app.post("/posts", addNewPost);
+
+    app.post("/posts/upload", upload.single("image"), imageUpload);
 }
 
 export default routes;
