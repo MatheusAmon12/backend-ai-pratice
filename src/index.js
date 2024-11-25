15bd041c919aada6f46e158c1665985f77f3753a
Matheus Amon <amonmatheus757@gmail.com>
2024-11-25 11:16:41 -0300

    REFACTOR: add env port variable

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
+import "dotenv/config";
 import express from "express";
 import routes from "./routes/posts.routes.js";
 
 const app = express();
 
-app.listen(3000, () => console.log("Server is running on port 3000"));
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log("Server is running on port 3000"));
 
 app.use(express.static("uploads"));
 
